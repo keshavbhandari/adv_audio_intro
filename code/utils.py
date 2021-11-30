8b1736c16b3f4ad7ca4421c0c809ba407c02f100
oreillyp <patrickoreilly175@gmail.com>
2021-11-30 18:09:22 +0000

    audio playback

--- code/utils.py
+++ code/utils.py
@@ -16,10 +16,65 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 
 from torchvision import datasets, transforms
+import IPython.display as ipd
+
+
+def play_audiomnist(x):
+    ipd.Audio(x.detach().cpu().numpy().flatten(), rate=16000)  # load a NumPy array
 
 
 def plot_audiomnist(x, y, model):
-    pass
+    """
+    Plot waveform and class scores side-by-side
+
+    :param x:
+    :param y:
+    :param model:
+    :return:
+    """
+
+    x = x.clone().detach().cpu()
+    if isinstance(y, torch.Tensor):
+        y = y.clone().detach().cpu()
+
+    # use model to compute class scores and predicted label
+    y_scores = torch.nn.functional.softmax(
+        model(x.reshape(1, 1, 16000)), dim=-1
+    )
+    y_pred = y_scores.argmax()
+
+    # initialize plot
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(6, 2.5))
+    width = 0.5
+    linewidth = 2.0
+
+    # image plot
+    axs[0].plot(x.squeeze().numpy(), 'k-')
+    axs[0].set_xlabel('Sample idx')
+    axs[0].set_ylabel('Amplitude')
+
+    # class scores plot
+    axs[1].bar(
+        list(range(0, 10)),
+        y_scores.flatten().detach().cpu().numpy(),
+        width,
+        color='black',
+        label='class scores',
+        edgecolor='black',
+        linewidth=linewidth
+    )
+
+    # formatting
+    fig.suptitle(f"True Label: {y}, Predicted Label: {y_pred}", y=1.1)
+    axs[1].grid(False)
+    axs[1].spines['left'].set_linewidth(linewidth)
+    axs[1].set_xlim(-1, 10)
+    axs[1].tick_params(bottom=True, left=True)
+    axs[1].set_yscale('log')
+    axs[1].set_xticks(list(range(0, 10)))
+    sns.despine(bottom=True)
+    plt.tight_layout()
+    plt.show()
 
 
 def train_audiomnist(model,
@@ -154,7 +209,7 @@ def test_audiomnist(model, device, test_loader):
 
     accuracy = 100 * correct / total
 
-    print(f"Model accuracy: {accuracy}")
+    print(f"\nModel accuracy: {accuracy}")
 
 
 def load_audiomnist(data_dir, train_batch_size: int = 64, test_batch_size: int = 128):
